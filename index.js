193216ac61b1b4cc1825510af2ed771a323cdd8b
Keegan McRoberts <keegan.mcroberts@gmail.com>
2022-06-10 08:42:47 -0500

    commit tree fbac1f1efeeb618fcb2f5f5acc7cd086ed1243a5

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Code your solution in this file!
+
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+//
+const returnFirstTwoDrivers = function(array) {
+    const newArray = drivers.slice(0,2)
+    return newArray;  
+}
+
+//
+const returnLastTwoDrivers = function(array) {
+    const driver = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+    const lastArray = driver.slice(2,4)
+    return lastArray;
+}
+//
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+
+//
+const createFareMultiplier = function(n){
+    return e => n * 5 
+}
+//
+const fareDoubler = createFareMultiplier(4)
+
+//
+const fareTripler = createFareMultiplier(7.2)
+
+
+
+
+const selectDifferentDrivers = (drivers, newFunction) => {
+    return newFunction(drivers);
+}
+
+   
+
+
